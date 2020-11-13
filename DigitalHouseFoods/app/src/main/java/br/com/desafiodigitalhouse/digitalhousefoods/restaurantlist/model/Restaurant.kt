3c5eacd7f5134412fc7591a42c9ef34db43d8a6a
emanuel tavares <emanuelfelipe30@gmail.com>
2020-11-13 12:15:06 -0300

    Adicionando o campo id no model Restaurant

--- DigitalHouseFoods/app/src/main/java/br/com/desafiodigitalhouse/digitalhousefoods/restaurantlist/model/Restaurant.kt
+++ DigitalHouseFoods/app/src/main/java/br/com/desafiodigitalhouse/digitalhousefoods/restaurantlist/model/Restaurant.kt
@@ -1,3 +1,3 @@
 package br.com.desafiodigitalhouse.digitalhousefoods.restaurantlist.model
 
-data class Restaurant(val name: String, val urlImage: String, val address: String, val closeHour: String)
+data class Restaurant(val id: Int, val name: String, val urlImage: String, val address: String, val closeHour: String)
